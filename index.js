f008728f1dbac0c260e686da80b65d706589387d
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2019-12-13 15:41:22 -0700

    makeSmartPerson challenge complete.

--- index.js
+++ index.js
@@ -61,9 +61,9 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
-}
+function getName(obj) {
+  return `Hello, my name is ${obj.name}`
+};
 
 /**
  * ### Challenge `makeSmartPerson`
@@ -78,13 +78,17 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
-}
-
-
-
-
+function makeSmartPerson(name) {
+  return obj = {
+    name: name,
+    sum(num1, num2){
+      return num1 + num2;
+    },
+    speak(){
+      return `Hello, my name is ${name}`;
+    }
+  }
+};
 
 /*
 // ⭐️ Example Test Data ⭐️
