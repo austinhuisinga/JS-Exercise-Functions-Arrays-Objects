94fc32c1b499957be83466b419c14ccf770b4113
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2019-12-15 09:58:05 -0700

    getGermanCars complete.

--- index.js
+++ index.js
@@ -252,8 +252,14 @@ function getOlderCars(inventory, maxyear) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  let germans = [];
+  for(let i = 0; i < inventory.length; i++) {
+    if(inventory[i].car_make == 'Audi' || inventory[i].car_make == 'Mercedes-Benz' || inventory[i].car_make == 'Volkswagen' || inventory[i].car_make == 'BMW') {
+      germans.push(inventory[i]);
+    }
+  }
+  return germans;
 }
 
 /**
